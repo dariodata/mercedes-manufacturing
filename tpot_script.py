4bd0635cf948dd9fe86f33a33a01a9179a0d5589
dariodata <dario86@gmail.com>
2017-06-27 16:10:49 +0200

    add submissions

--- tpot_script.py
+++ tpot_script.py
@@ -15,7 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(train.drop('y', axis=1), tra
                                                     train_size=0.75, test_size=0.25)
 
 pipeline_optimizer = TPOTRegressor(generations=3, population_size=5, cv=5,
-                                    random_state=42, verbosity=2)
+                                    random_state=42, verbosity=2, warm_start=True)
 pipeline_optimizer.fit(X_train, y_train)
 print(pipeline_optimizer.score(X_test, y_test))
 pipeline_optimizer.export('tpot_exported_pipeline.py')
